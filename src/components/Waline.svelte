8ea90d40018b9ae7d0c5b4168c14e453462168c0
Gabriel B <guiqide@gmail.com>
2025-11-21 11:20:38 +0800

    feat: new post

---
+++ src/components/Waline.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { init } from '@waline/client';
+  import '@waline/client/waline.css';
+  import { onMount } from 'svelte';
+
+  export let serverURL;
+  export let path = window.location.pathname;
+
+  onMount(() => {
+    init({
+      el: '#waline',
+      serverURL,
+      path,
+    });
+  });
+</script>
+
+<div id="waline"></div>
